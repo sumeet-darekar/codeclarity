f85e456b42ae29c497213febf20d9469a71cbe3b
sumeet <79735377+sumeet-darekar@users.noreply.github.com>
2025-01-28 08:52:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def main():
 
     st.title("CodeClarity")
     st.write("""
-    This tool formats minified JavaScript code, extracts links, and identifies potential credentials. 
+    [r1] This tool formats minified JavaScript code, extracts links, and identifies potential credentials. 
     Upload a file containing minified code or paste your code directly.
     """)
 
